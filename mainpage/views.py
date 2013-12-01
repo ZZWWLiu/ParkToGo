9a4214792798034be77232ee2a40f410241e2a45
ZZWWLiu <ziwei.liu1020@gmail.com>
2013-12-01 15:37:24 -0600

    modify output

--- mainpage/views.py
+++ mainpage/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from mainpage.models import UserParkForm
 import urllib2
 from xml.dom import minidom
-# from django.core.cache import cache
 import logging
+import time
 import pyipinfodb
 from core_algorithm import recommender
 from google.appengine.api import memcache
@@ -13,13 +13,39 @@ from google.appengine.api import memcache
 # photo url = http://www.reserveamerica.com/webphotos/CO/pid50032/5/180x120.jpg
 # src = http://www.reserveamerica.com + webphoto
 
-def generateQuery(form):
-	out = 'pstate='+form['state']
-	if form['siteType'] != '':
-		out += '&siteType=' + form['siteType']
-	if form['Amenity'] != '':
-		out += '&amenity=' + form['Amenity']
-	return out
+
+# it's better to store all the parks info in database
+def getResDetail(res):
+	key = 'd2rttztqpfhbqjz42buq6duc'
+	baseUrl = 'http://www.reserveamerica.com'
+	resDetail = []
+	for r in res:
+		detail = {}
+		query = 'contractCode='+r['state']+'&parkId='+r['facilityID']
+		camp_detail = 'http://api.amp.active.com/camping/campground/details?'+query+'&api_key='+key
+		urlfile = urllib2.urlopen(camp_detail)
+		data = urlfile.read()
+		urlfile.close()
+		dom = minidom.parseString(data)
+		detailDescription = dom.getElementsByTagName('detailDescription')
+		detail['description'] = detailDescription[0].attributes['description'].value
+		detail['facility'] = detailDescription[0].attributes['facility'].value
+		detail['importantInformation'] = detailDescription[0].attributes['importantInformation'].value
+		photoUrls = []
+		photos = dom.getElementsByTagName('photo')
+		for p in photos:
+			realUrl = p.attributes['realUrl'].value
+			# logging.error(realUrl)
+			if realUrl != '':
+				photoUrls.append(baseUrl+realUrl)
+		detail['imgs'] = photoUrls
+		detail['state'] = r['state']
+		resDetail.append(detail)
+		time.sleep(0.6)
+	return resDetail
+
+
+
 
 def homepage(request):
 	if request.method == 'GET':
@@ -28,7 +54,7 @@ def homepage(request):
 		# ip = ipinfo.GetPublicIp()
 		ip = '165.91.11.22'
 		memcache.set('ip', ip)
-		logging.error(ip)
+		# logging.error(ip)
 		# use clients' public ip address to find their locations
 		dataDict = memcache.get(ip)
 		if dataDict is None:
@@ -36,7 +62,7 @@ def homepage(request):
 			memcache.set(ip, dataDict)
 		lat = dataDict["latitude"]
 		lon = dataDict["longitude"]
-		# logging.error(dataDict)
+		logging.error(dataDict)
 		API_KEY = 'AIzaSyAnEt9j1iiUDG6X2cRxQ2GUfotwoe4vCCY'
 		google_maps = "https://maps.googleapis.com/maps/api/js?key="+API_KEY+"&sensor=false"
 		content = {'google_maps_src': google_maps ,
@@ -51,14 +77,15 @@ def submit(request):
 		if form.is_valid():
 			user_need = form.cleaned_data
 			ip = memcache.get('ip')
-			logging.error(ip)
+			# logging.error(ip)
 			dataDict = memcache.get(ip)
 			lat = dataDict["latitude"]
 			lon = dataDict["longitude"]
-			res = recommender.recommend(3, float(lat), float(lon))
+			res = recommender.recommend(0, float(lat), float(lon))
+			resDetail = getResDetail(res)
 			API_KEY = 'AIzaSyAnEt9j1iiUDG6X2cRxQ2GUfotwoe4vCCY'
 			google_maps = "https://maps.googleapis.com/maps/api/js?key="+API_KEY+"&sensor=false"
-			content = {'results': res,
+			content = {'results': resDetail,
 					   'google_maps_src': google_maps ,
 		               'latitude' : float(lat),
 		               'longitude' : float(lon),
